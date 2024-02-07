7e86c832f3fc1c31f62cb17387626eaddf2b13d4
HASSAN ED-DYB <107430322+hassandyb@users.noreply.github.com>
2024-02-07 19:53:09 +0100

    Add files via upload

--- cpp05/ex00/Bureaucrat.cpp
+++ cpp05/ex00/Bureaucrat.cpp
@@ -6,12 +6,20 @@
 /*   By: hed-dyb <hed-dyb@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 16:05:09 by hed-dyb           #+#    #+#             */
-/*   Updated: 2024/02/07 14:05:00 by hed-dyb          ###   ########.fr       */
+/*   Updated: 2024/02/07 19:52:25 by hed-dyb          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
+// Canonical form : -----------------------
+
+// Bureaucrat();Bureaucrat(const std::string & name, int grade);
+// Bureaucrat(const Bureaucrat & other);
+// Bureaucrat & operator=( const Bureaucrat & other);
+// ~Bureaucrat();
+
+// getters : -----------------------
 std::string Bureaucrat::getName()
 {
 	return this->name;
@@ -21,6 +29,27 @@ int Bureaucrat::getGrade()
 {
 	return this->grade;
 }
+
+
+
+// Exceptions implimentaion: -----------------------
+
+class	GradeTooHighException : public std::exception
+{
+	virtual const char * what() const throw()
+	{
+		return ("Exception : attempting to reach the highest grade occurred, please note that you highest grade you can get is one.");
+	}
+}
+
+class	GradeTooLowException : public std::exception
+{
+	virtua const char * what() const throw()
+	{
+		return ("Exception : attempting to reach the lowest  grade occurred, please note that you lowest grade you can get is 150.");
+	}
+}
+
 
 void Bureaucrat::incrementGrade()
 {
--- cpp05/ex00/Bureaucrat.hpp
+++ cpp05/ex00/Bureaucrat.hpp
@@ -6,14 +6,14 @@
 /*   By: hed-dyb <hed-dyb@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 17:37:51 by hed-dyb           #+#    #+#             */
-/*   Updated: 2024/02/07 14:05:24 by hed-dyb          ###   ########.fr       */
+/*   Updated: 2024/02/07 19:52:01 by hed-dyb          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUREAUCRAT_HPP
 #define BUREAUCRAT_HPP
 
-#include <iosream>
+#include <iostream>
 
 class Bureaucrat
 {
@@ -22,14 +22,23 @@ class Bureaucrat
 		int grade;
 	public :
 		Bureaucrat();
+		// we used the keyword const for name and other cause the function pomisses not to change them, cause actually it should't.
+		Bureaucrat(const std::string & name, int grade);
+		Bureaucrat(const Bureaucrat & other);
+		Bureaucrat & operator=( const Bureaucrat & other);
+		~Bureaucrat();
+		
 		
 
 		
 		std::string getName();
-		std::string getGrade();
+		int getGrade();
 		
 		void incrementGrade();
 		void decrementGrade();
+		
+		class	GradeTooHighException : public std::exception;
+		class	GradeTooLowException : public std::exception;
 		
 		
 }
