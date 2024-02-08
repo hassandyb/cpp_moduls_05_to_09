c9299dea57c8ddc82011a5bfde9fae00109b6aaf
HASSAN ED-DYB <107430322+hassandyb@users.noreply.github.com>
2024-02-08 16:29:26 +0100

    Add files via upload

---
+++ cpp05/ex01/Form.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Form.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hed-dyb <hed-dyb@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/08 16:10:42 by hed-dyb           #+#    #+#             */
+/*   Updated: 2024/02/08 16:28:36 by hed-dyb          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FORM_HPP
+#define FORM_HPP
+
+#include <iostream>
+
+class Form
+{
+	private :
+		const std::string name;
+		bool signed;
+		const int gradetosign;
+		const int gradetoexecute;
+	public :
+	// canonical form .....
+};
+
+
+#endif
